dc9a5222fa09b16af3b852f0c20883f6c3950ef2
Gamerhort <sachintambe907@gmail.com>
2023-08-06 12:58:25 +0530

    atomatic commit example

---
+++ ato_promo_overlo.java
@@ -0,0 +1,28 @@
+class  AtomaticPramotion {
+    public void promotion1(double a ) { // |
+        System.out.println("double"+a); // |
+    }                                   // |
+    public void promotion1(int a ) {    // |  JAB HAM IS ME SE KOI BHI DELETE MARENGE TAB ATOMATIC PROMATION WALI IMAGE KI DIAGRAM KI ORDER ME OTHERE VARIABLE PAR ATOMATI SHIFT KIYA JAYEGA 
+        System.out.println("int"+a);    // |
+    }                                   // |
+    public void promotion1(char a ) {   // |
+        System.out.println("char"+a);   // |
+    }                                   // |
+    public void promotion1(float a ) {  // |
+        System.out.println("float"+a);  // |
+    }                                   // |
+    public void promotion1(short a ) {  // |
+        System.out.println("short"+a);  // |
+    }                                   // |
+    public void promotion1(byte a ) {   // |
+        System.out.println("byte"+a);   // |_
+    }
+
+
+}
+public class ato_promo_overlo {
+    public static void main (String [] args ) {
+       AtomaticPramotion pramotion = new AtomaticPramotion();
+       pramotion.promotion1('c');
+    }
+}
