c6b99c7fc6b8575afe589f15b7d0a5deac9bd911
Gamerhort <sachintambe907@gmail.com>
2023-08-12 17:19:00 +0530

    length vs length()

---
+++ LengthVsLengt.java
@@ -0,0 +1,16 @@
+public class LengthVsLengt {
+    public static void main (String [] args ) {
+        // length - property used in arrays 
+    int [] a = {45 , 78 , 58 , 96 };
+    System.out.println(a.length);
+    int [] [] b = {
+        {45 , 85 , 96 } ,
+        {45 , 75 , 78 }
+    } ;
+    System.out.println(b.length);
+    System.out.println(b[0].length);
+    // length() - method used in Strings
+    String n =  "sachin" ;
+    System.out.println(n.length());
+    }
+}
